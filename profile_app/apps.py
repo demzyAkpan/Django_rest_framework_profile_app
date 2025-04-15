0733f5883d05932d9c403cf6e46731d4f1cbfe4f
demzyAkpan <demzyukonta@gmail.com>
2025-04-15 15:54:38 +0100

    Add signals to create and save user profiles on User model events

--- profile_app/apps.py
+++ profile_app/apps.py
@@ -4,3 +4,10 @@ from django.apps import AppConfig
 class ProfileAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'profile_app'
+
+    def ready(self):
+        import profile_app.signals
+        # This will ensure that the signals are imported when the app is ready
+        # and that the signal handlers are connected to the appropriate signals.
+        # This is important for ensuring that the signals work correctly.
+        # The import statement is placed here to avoid circular imports.
