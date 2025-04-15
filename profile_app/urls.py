32a356664619ab115778c257c2bdcac5b67ae8a0
demzyAkpan <demzyukonta@gmail.com>
2025-04-15 16:52:45 +0100

    setup app urls

--- profile_app/urls.py
+++ profile_app/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
+from views import UserProfileList, UserProfileDetail, Follow, FollowDelete
 
+urlpatterns =[ 
 
-urlpatterns =[
+   path('profiles/', UserProfileList.as_view(), name='user-profile-list'),#create a new profile
+   path('profiles/<int:pk>/', UserProfileDetail.as_view(), name='user-profile-detail'),#get a specific profile
+   path('profiles/<int:pk>/follow/', Follow.as_view(), name='follow-user'),#follow a user
+   path('profiles/<int:pk>/unfollow/', FollowDelete.as_view(), name='unfollow-user'),#unfollow a user
+   path('profiles/<int:pk>/followers/', UserProfileDetail.as_view(), name='user-followers'),#get followers of a user
    
 ]
