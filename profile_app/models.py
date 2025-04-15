bcdc5f5085d56ada63183695b218f77bf33671c1
demzyAkpan <demzyukonta@gmail.com>
2025-04-15 10:02:37 +0100

    created profile model

--- profile_app/models.py
+++ profile_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
 
-# Create your models here.
+
+class Profile(models.Model):
+    username = models.ForeignKey(User, on_delete=models.CASCADE, unique=True)
+    bio = models.TextField(blank=True)
+    following = models.ForeignKey(User, on_delete=models.CASCADE, related_name='followers')
+    follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name='following')
+    date_joined = models.DateField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('follower, following') #prevents duplicate follows
+
+    def __str__(self):
+        return f'{self.username}'
+
+
+
+
